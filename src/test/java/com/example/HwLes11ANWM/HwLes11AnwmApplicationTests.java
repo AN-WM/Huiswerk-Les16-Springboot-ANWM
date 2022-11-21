78b67821ed5e4fd178005b076331f6fcd2583913
AN_WM <an.meester@yahoo.com>
2022-11-21 15:12:44 +0100

    First commit, empty

---
+++ src/test/java/com/example/HwLes11ANWM/HwLes11AnwmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.HwLes11ANWM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HwLes11AnwmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
